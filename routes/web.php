700b9317096ad56bd95bbc0bdaf9083ab8c87d6c
Medha Kumari <medha.kumari321@gmail.com>
2020-06-19 21:06:36 +0530

    second commit

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/questionnaires/create', 'QuestionnaireController@create');
 Route::post('/questionnaires', 'QuestionnaireController@store');
 Route::get('/questionnaires/{questionnaire}', 'QuestionnaireController@show');
+Route::get('/questionnaires/{questionnaire}/questions/create','QuestionController@create');
+Route::post('/questionnaires/{questionnaire}/questions','QuestionController@store');
